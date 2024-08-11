c09c046abd10bec80f2d23b170a7611d071955da
JoudBinHarkan <joudbinharkan@gmail.com>
2024-08-11 16:27:38 +0300

    added authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+//import { Amplify } from "aws-amplify";
+//import outputs from '../amplify_outputs.json'; // Correct path if `main.tsx` is in `src`
+//import awsConfig from './aws-exports'; // Import AWS configuration
+//import awsExports from './aws-exports';  // Make sure this path is correct
 
-Amplify.configure(outputs);
 
+
+
+//Amplify.configure(awsExports);
+///Amplify.configure(outputs);
+//Amplify.configure(awsConfig);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
