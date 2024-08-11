7407eea067d9c787ab50225c2a4c8aa07691af3a
JoudBinHarkan <joudbinharkan@gmail.com>
2024-08-11 18:48:41 +0300

    log in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { AmplifyProvider } from '@aws-amplify/ui-react';
-import awsExports from './aws-exports';
-import { Amplify } from 'aws-amplify';
-
-// Configure Amplify with your AWS exports
-Amplify.configure(awsExports);
 
 const client = generateClient<Schema>();
 
@@ -17,46 +10,37 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    // Subscribe to changes in the Todo data
-    const subscription = client.models.Todo.observeQuery().subscribe({
+    client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-
-    // Clean up subscription on unmount
-    return () => subscription.unsubscribe();
   }, []);
 
   function createTodo() {
-    const content = window.prompt("Todo content");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
+    client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   return (
-    <AmplifyProvider>
-      <Authenticator>
-        {({ signOut }) => (
-          <main>
-            <h1>My todos</h1>
-            <button onClick={createTodo}>+ new</button>
-            <ul>
-              {todos.map((todo) => (
-                <li key={todo.id}>{todo.content}</li>
-              ))}
-            </ul>
-            <button onClick={signOut}>Sign out</button>
-            <div>
-              🥳 App successfully hosted. Try creating a new todo.
-              <br />
-              <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                Review next step of this tutorial.
-              </a>
-            </div>
-          </main>
-        )}
-      </Authenticator>
-    </AmplifyProvider>
+    <Authenticator>
+      {({ signOut }) => (
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <button onClick={signOut}>Sign out</button>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Amplify } from 'aws-amplify';
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 //import { Amplify } from "aws-amplify";
-//import outputs from '../amplify_outputs.json'; // Correct path if `main.tsx` is in `src`
-//import awsConfig from './aws-exports'; // Import AWS configuration
-//import awsExports from './aws-exports';  // Make sure this path is correct
+import outputs from '../amplify_outputs.json'; // Correct path if `main.tsx` is in `src`
+import awsConfig from './aws-exports'; // Import AWS configuration
+import awsExports from './aws-exports';  // Make sure this path is correct
 
 
-
+Amplify.configure(awsExports);
 
 //Amplify.configure(awsExports);
-///Amplify.configure(outputs);
-//Amplify.configure(awsConfig);
+Amplify.configure(outputs);
+Amplify.configure(awsConfig);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
